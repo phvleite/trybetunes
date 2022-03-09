0911625c75ec7b4a8cc9c38649f2f853f9005036
phvleite <phvleite@gmail.com>
2022-03-08 23:48:17 -0300

    Requisito 13 finalizado

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import Header from '../component/Header';
+import Loading from '../component/Loading';
+import MusicCard from '../component/MusicCard';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      load: true,
+      favoritesList: [],
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ load: true }, async () => {
+      const favoritesList = await getFavoriteSongs();
+      this.setState({
+        load: false,
+        favoritesList });
+    });
+  }
+
+  updateFavoriteSongs = async () => {
+    const favoritesSongs = await getFavoriteSongs();
+    this.setState({
+      favoritesList: favoritesSongs,
+    });
+  }
+
+  toggleLoad = () => {
+    this.setState((prevState) => ({
+      load: !prevState.load,
+    }));
+  }
+
   render() {
+    const { load, favoritesList } = this.state;
     return (
-      <div data-testid="page-favorites">
+      <div data-testid="page-album">
         <Header />
-        <h2>Favorites</h2>
+        { load ? <Loading /> : (
+          <div>
+            {favoritesList.map((track, index) => (
+              index > 0
+               && <MusicCard
+                 track={ track }
+                 key={ track.trackId }
+                 favoritesList={ favoritesList }
+                 updateFavoriteSongs={ this.updateFavoriteSongs }
+                 toggleLoad={ this.toggleLoad }
+               />
+            ))}
+          </div>
+        )}
       </div>
     );
   }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../component/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../component/Loading';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuario: {},
+      load: false,
+    };
+  }
+
+  componentDidMount() {
+    this.returnGetUser();
+  }
+
+  async returnGetUser() {
+    this.setState(
+      { load: true },
+      async () => {
+        const user = await getUser();
+        this.setState({
+          load: false,
+          usuario: user,
+        });
+      },
+    );
+  }
+
   render() {
+    const { usuario, load } = this.state;
+    const { name, email, image, description } = usuario;
     return (
       <div data-testid="page-profile">
         <Header />
         <h2>Profile</h2>
+        { load
+          ? <Loading />
+          : (
+            <>
+              <img src={ image } data-testid="profile-image" alt={ `Usuário: ${name}` } />
+              <span>{name}</span>
+              <span>{email}</span>
+              <span>{description}</span>
+              <div>
+                <Link exact to="/profile/edit">Editar perfil</Link>
+              </div>
+            </>
+          ) }
       </div>
     );
   }
